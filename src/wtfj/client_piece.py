5510f80b8aad5c88cec9170dcd35318db04d9c1b
maxlazarus <max.prokopenko@gmail.com>
2017-02-27 00:56:03 -0800

    Fail silently on interest function absent

--- src/wtfj/client_piece.py
+++ src/wtfj/client_piece.py
@@ -1,6 +1,7 @@
 import time
 from threading import Thread
 from Queue import Queue,Empty
+import traceback
 
 try:
 	DONTWAIT = zmq.DONTWAIT
@@ -15,11 +16,12 @@ class ClientPiece(object):
 		self._STOP_MSG = 'stopping'
 		self._START_MSG = 'started'
 		self._alive = False
+		self.flags = {}
 
-	def start(self,subscriber,pusher,interests=[],echo=False):
+	def start(self,subscriber,pusher,echo=False):
 		self._birthday = time.clock()
 		self._echo = echo
-		self._interests = interests
+		self._interests = []
 		self._sub = subscriber
 		self._push = pusher
 		self._alive = True
@@ -45,6 +47,9 @@ class ClientPiece(object):
 	def uptime(self):
 		return time.clock() - self._birthday
 
+	def add_interest(self,interest):
+		self._interests.append(interest)
+
 	def _poll(self):
 		while self._alive == True:
 			msg = self._sub.recv_string(DONTWAIT,self._Uid)
@@ -52,6 +57,7 @@ class ClientPiece(object):
 				if self._interpret(msg) == False:
 					if self._echo == True:
 						self._push.send_string(msg)
+			time.sleep(0.001)
 
 	def _on_marco(self):
 		self.send('polo',str(self.uptime()))
@@ -71,6 +77,7 @@ class ClientPiece(object):
 	def _interpret(self,msg):
 		self._last_msg_time = time.clock()
 		ret = False
+		fail_silent = False
 		parts = msg.split(' ',2)
 		try:
 			size = len(parts)
@@ -84,15 +91,18 @@ class ClientPiece(object):
 						getattr(self,'_on_'+parts[1])()
 					ret = True
 				elif parts[0] in self._interests:
+					# Fail silently when receiving a message from an interest as most of these
+					# will not be mapped to functions to handle them
+					fail_silent = True
 					if size == 3:
 						getattr(self,'_on_'+parts[0]+'_'+parts[1])(parts[2])
 					else:
 						getattr(self,'_on_'+parts[0]+'_'+parts[1])()
 					ret = True
 		except AttributeError as e:
-			self.err('no interpretation of message ['+msg+'] available')
+			if not fail_silent: self.err('no interpretation of message ['+msg+'] available')
 		except Exception as e:
-			self.err('exception '+repr(e))
+			self.err('exception thrown\n'+traceback.format_exc())
 		return ret
 
 
@@ -104,6 +114,8 @@ class MyPiece(ClientPiece):
 			self.send('foo','with data '+data)
 
 	def _after_start(self):
+		self.size = None
+		self.add_interest('gui')
 		self.send_to('gui','get','size')
 		self.send('_after_start','-this function called after successful start of piece')
 
@@ -111,10 +123,11 @@ class MyPiece(ClientPiece):
 		self.send('BARDYBARBAR')
 
 	def _before_stop(self):
-		self.send('_before_stop','-this function called before attempting to stop piece '+self._Uid)
+		self.send('_before_stop','-this function called before attempting to stop piece')
 
 	def _on_gui_size(self,data=None):
-		self.send('size '+str(data)+' received')
+		#self.size = [int(x) for x in data.split[',',2][0:1]]
+		self.send('ack','gui size')
 
 
 class MockGuiPiece(ClientPiece):
@@ -127,9 +140,10 @@ class MockGuiPiece(ClientPiece):
 
 
 class ServerPiece(object):
-	def __init__(self):
+	def __init__(self,echo=False):
 		self._pull = Queue()
 		self._pub = {}
+		self._echo = echo
 
 	def send_string(self,string):
 		''' Sends a string to this server '''
@@ -155,26 +169,29 @@ class ServerPiece(object):
 		''' Puts message in outgoing queue '''
 		Uid = string.split(' ',1)[0]
 		try:
-			self._pub[Uid].put(string)
+			found = self._pub[Uid]
 		except KeyError:
 			self._pub[Uid] = Queue()
-			self._pub[Uid].put(string)
+		for key in self._pub:
+			self._pub[key].put(string)	
 		print('[sent] '+string)
 
 	def pull(self,args=None):
 		''' Returns messages in incoming queue '''
 		ret = self._pull.get(block=(args != DONTWAIT))
 		print('[recvd] '+ret)
+		if self._echo == True:
+			self.publish(ret)
 		return ret
 
-serv = ServerPiece()
+serv = ServerPiece(echo=True)
 
 MockGuiPiece('gui').start(serv,serv)
-serv.pull()
 serv.publish('@gui marco')
 serv.pull()
 
-MyPiece('max').start(serv,serv,'gui')
+MyPiece('max').start(serv,serv)
+serv.pull()
 serv.pull()
 serv.pull()
 serv.pull()
@@ -190,4 +207,15 @@ serv.pull()
 serv.pull()
 
 serv.publish('@gui stop')
+serv.pull()
+
+MyPiece('max').start(serv,serv)
+serv.publish('max laz ')
+serv.pull()
+serv.pull()
+serv.pull()
+serv.pull()
+
+serv.publish('@max stop')
+serv.pull()
 serv.pull()
