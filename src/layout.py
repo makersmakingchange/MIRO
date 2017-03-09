122e1a3c16edb826d81bca6847bfe3ca393e0773
alexb <alexberlingeri@yahoo.com>
2017-03-09 15:52:06 -0800

    Screen division algorithm added to layout module.

--- src/layout.py
+++ src/layout.py
@@ -17,6 +17,43 @@ class Layout(Piece):
 			for i in range(self._n_current_keys):
 				self.send_to(Uid.TKPIECE,Req.DELETE,'key'+str(i))
 			self._n_current_keys = 0
+			max_rows = 5
+			shape_type = "rect"
+			assert(max_rows == 5)
+			if (n > max_rows):
+				cols = n/max_rows + (n%max_rows > 0)
+			elif(n == max_rows):
+				cols = n/(max_rows/2) + (n%(max_rows/2) > 0) - 1
+			else:
+				cols = n/2 + (n%2 > 0)
+			col_keys = []
+			for x in range(cols):
+				col_keys.append(1)
+			i = 0
+			while(sum(col_keys) != n):
+				col_keys[i%len(col_keys)] += 1
+				i+=1
+
+			self.shape_list = ""
+			dx = 1.0 / (len(col_keys))
+			i = 0
+			key_counter = 0
+			for val in reversed(col_keys):
+				dy = 1.0 / (val)
+				j = 1
+				for x in range(0,val):
+					ul = (i*dx,x*dy)
+					br = (min(1,(i+1)*dx),min(1,(x+1)*dy))
+					self.shape_list = self.shape_list + shape_type + "," + str(ul[0]) + "," + str(ul[1]) + "," + str(br[0]) + "," + str(br[1]) + ","
+					x = (ul[0] + br[0])/2
+					y = (ul[1] + br[1])/2
+					self.send_to(Uid.TKPIECE,Req.CREATE,'text,key'+str(key_counter)+','+str(x)+','+str(y))
+					j+=1
+					key_counter += 1
+				i+=1
+			self.shape_list = self.shape_list[0:len(self.shape_list)-1]
+			self._n_current_keys = n
+
 		# For each option emitted by engine
 		for i in range(n):
 			# Determine the center point of the screen to draw it
@@ -41,9 +78,19 @@ class Layout(Piece):
 					self.send_to(Uid.TKPIECE,Req.CREATE,'text,key'+str(i)+','+x+','+y)
 				# Update the value of the text field
 				self.send_to(Uid.TKPIECE,Msg.TEXT,'key'+str(i)+','+options[i])
-		# Save current number of options displayed and send acknowledgement back
+
+		# Save current number of options displayed and send acknowledgement back				
 		self._n_current_keys = n
 		self.send(Msg.ACK)
+
+	@staticmethod
+	def script():
+		text_entry = [
+			'@layout marco',
+			'engine options 1,2,3,4',
+			'@layout stop'
+		]
+		return Script(text_entry)
 
 if __name__ == '__main__':
 	from sys import argv
