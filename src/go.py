5ae3f000c71e3d0a5ee22963f69a0f1cd3687016
Max Prokopenko <max@theprogrammingclub.com>
2017-03-10 17:11:11 -0800

    Working on wface

--- src/go.py
+++ src/go.py
@@ -1,8 +1,5 @@
 from wtfj import *
 
-try:
-	Runner.run(Uid.SYSTEM,Mode.ZSERVER)
-	Runner.run(Uid.ZCONSOLE)
-	Runner.run(Uid.ZPRINTER)
-except Exception as e:
-	print(repr(e))
+Runner.run(Uid.SYSTEM,Mode.ZSERVER)
+Runner.run(Uid.ZCONSOLE)
+Runner.run(Uid.ZPRINTER)
