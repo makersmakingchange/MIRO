31aafdeb6ef74ef58750ae053ce43732395f6426
alexb <alexberlingeri@yahoo.com>
2017-03-19 10:24:12 -0700

    Images added to menu.

--- src/engine.py
+++ src/engine.py
@@ -7,30 +7,25 @@ def main():
 letters_lc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 numbers = ['0','1','2','3','4','5','6','7','8','9']
 punctuation = ['spc','.','com','\'','\"','?','!',';','-',':','(',')','num','$','[',']','{','}','/','\\']
-#menu_options = ['#keyboard','#menu','#undo']
 menu_options = ['#keyboard','edit']
 menu_handles = {}
-keyboard_options = ['a_to_z','0_to_9','...']
+keyboard_options = ['#alphabet','#numbers','...']
 keyboard_handles = {}
-edit_options = ['save']
+edit_options = ['#save','#undo','edit']
 
 class Engine(Piece):
 	''' Letter and menu selection engine '''
 
 	def _ON_build(self,data):
-		#self._options = OptionNode()
 		num_options = int(data)
 		if (num_options == 1):
 			self.send(Msg.ERR,'1 key layout invalid\n')
 		else:
-			#build_non_ordered_tree(self._options,num_options,menu_options,menu_handles)
-			#build_non_ordered_tree(menu_handles.get('#keyboard'),num_options,keyboard_options,keyboard_handles)
-			#build_ordered_tree(keyboard_handles.get('a_to_z'),num_options,letters_lc)
 			self._options = OptionNode()
 			build_non_ordered_tree(self._options,num_options,menu_options,menu_handles)
 			build_non_ordered_tree(menu_handles.get('#keyboard'),num_options,keyboard_options,keyboard_handles)
-			build_ordered_tree(keyboard_handles.get('a_to_z'),num_options,letters_lc)
-			build_ordered_tree(keyboard_handles.get('0_to_9'),num_options,numbers)
+			build_ordered_tree(keyboard_handles.get('#alphabet'),num_options,letters_lc)
+			build_ordered_tree(keyboard_handles.get('#numbers'),num_options,numbers)
 			build_non_ordered_tree(keyboard_handles.get('...'),num_options,punctuation)
 			build_non_ordered_tree(menu_handles.get('edit'),num_options,edit_options)
 			self._current_option = self._options
@@ -43,12 +38,10 @@ class Engine(Piece):
 
 	def _ON_process(self,data):
 		msg = ''
-		if len(self._current_option.children) == 0 and self._current_option.content[0] != '#':
-			if (self._current_option.content == 'save'):
-				self.send(Msg.COMMIT, 'True')
-			else:	
+		if len(self._current_option.children) == 0:
+			if (self._current_option.content[0] != '#'):
 				self.send_to(Uid.AUDIO,Req.SPEAK,self._current_option.content)
-				self.send(Msg.CHOSE, self._current_option.content)
+			self.send(Msg.CHOSE, self._current_option.content)
 			self._current_option = self._options
 		for i in range(len(self._current_option.children)):
 			msg += self._current_option.children[i].content
@@ -62,10 +55,13 @@ class Engine(Piece):
 			'@engine marco',
 			'@engine period 1',
 			'@engine build 3',
+			'@engine select 1',
 			'@engine select 0',
-			'@engine select 1',
-			'@engine select 1',
-			'@engine select 1',
+			'@engine select 0',
+			'@engine select 0',
+			'@engine select 0',
+			'@engine select 0',
+			'@engine select 0',
 			'@engine stop'
 		])
 
