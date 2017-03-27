48e8e867e71bb49ffd999a9e0181471bdc3f5104
maxlazarus <max.prokopenko@gmail.com>
2017-03-26 23:27:40 -0700

    Working on speaking unknown words

--- src/audio.py
+++ src/audio.py
@@ -1,16 +1,24 @@
 import winsound
+import pyttsx
 from wtfj import *
 
 AUDIO_PATH = '../res/audio/'
 
 class Audio(Piece):
-	
+
+	def _BEFORE_start(self):
+		engine = pyttsx.init()
+		engine.startLoop(False)
+		engine.setProperty('rate',100)
+		self._engine = engine
+
 	def _ON_speak(self,data=None):
 		if data[0] is '#': data = data[1:]
 		if data in ['a_to_m','n_to_z','space','undo','next','menu','keyboard','to'] or data in 'abcdefghijklmnopqrstuvwxyz':
 			winsound.PlaySound(AUDIO_PATH+data+'_sound.wav',winsound.SND_FILENAME)
 		else:
 			self.err('Could not find file for argument ['+str(data)+']')
+			self._engine.say(data)
 
 	@staticmethod
 	def script():
@@ -26,6 +34,7 @@ class Audio(Piece):
 			'@audio speak a',
 			'@audio speak a_to_m',
 			'@audio speak #undo',
+			'@audio speak hello my honey',
 			'@audio uptime',
 			'@audio stop'
 		]
