4f9d1df1d63a69ece5b0711591224ece247d6e91
maxlazarus <max.prokopenko@gmail.com>
2017-03-28 22:44:36 -0700

    Fixed windows speech, added mouth output to facial detection

--- log/filter.py
+++ log/filter.py
@@ -1,9 +1,21 @@
 fout = open('eyebrows1.csv','w')
-with open('20170325_162429.txt','r') as fin:
+with open('20170326_110635.txt','r') as fin:
 	for line in fin:
+		if 'face' in line:
+			parts = line.split()
+			try:
+				data = parts[3].split(',')
+				assert len(data) == 6
+				output = parts[0]+','+parts[3]
+				print(output)
+				fout.write(output)
+			except Exception:
+				pass
+		'''
 		if 'wface text' in line:
 			parts = line.split()
 			data = parts[3].split(',')
 			output = data[0]+','+data[1]+','+data[2]+'\n'
 			print(output)
 			fout.write(output)
+		'''
--- src/audio.py
+++ src/audio.py
@@ -1,41 +1,40 @@
 import winsound
-import pyttsx
 from wtfj import *
 from os import system
+import win32com.client as wincl # pip install pypiwin32
 
 AUDIO_PATH = '../res/audio/'
 
-class Audio(Piece):
+speak = wincl.Dispatch("SAPI.SpVoice")
+speak.Speak('')
+speak.Rate = -2
 
-	def _BEFORE_start(self):
-		engine = pyttsx.init()
-		engine.startLoop(False)
-		engine.setProperty('rate',100)
-		self._engine = engine
+
+class Audio(Piece):
+	''' Searches for prerecorded wavs of argument passed to it. If not found calls text-to-speech conversion '''
 
 	def _ON_speak(self,data=None):
 		if data[0] is '#': data = data[1:]
 		if data in ['a_to_m','n_to_z','space','undo','next','menu','keyboard','to'] or data in 'abcdefghijklmnopqrstuvwxyz':
 			winsound.PlaySound(AUDIO_PATH+data+'_sound.wav',winsound.SND_FILENAME)
 		else:
-			self.err('Could not find file for argument ['+str(data)+']')
-			self._engine.say(data)
+			self.err('Could not find file for argument ['+str(data)+']')			
+			speak.Speak(data)
 
 	@staticmethod
 	def script():
 
 		script = [
 			'@audio uptime',
-			'@audio period 1',
 			'@audio marco',
-			'@audio speak a',
-			'@audio speak a_to_m',
+			'@audio period 3',
+			'@audio speak anything I want to say',
+			'@audio speak hello my honey',
 			'@audio speak #undo',
 			'@audio period 0.2',
 			'@audio speak a',
 			'@audio speak a_to_m',
 			'@audio speak #undo',
-			'@audio speak hello my honey',
 			'@audio uptime',
 			'@audio stop'
 		]
